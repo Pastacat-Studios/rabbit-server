53d7c7c52b294537fea1b41435739b8cd159419b
insert <insert@insertapp.net>
2025-12-28 23:20:13 -0500

    feat: prevent bad usernames

--- gamehandler/togame.go
+++ gamehandler/togame.go
@@ -2,11 +2,34 @@ package gamehandler
 
 import (
 	"pastacat/rabbitserver/database"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
 
+const Version string = "0.1a"
+
+type connection struct {
+	Ver string `json:"version"`
+	Id  string `json:"id"`
+}
+
 func PongGame(c *gin.Context) {
+	var con connection
+	err := c.BindJSON(&con)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	if con.Ver != Version {
+		c.JSON(400, gin.H{"error": "Incompatible Version"})
+		return
+	}
+	match, _ := regexp.MatchString(`^[a-zA-Z]+$`, con.Id)
+	if !match {
+		c.JSON(406, gin.H{"error": "Bad Username"})
+		return
+	}
 	c.JSON(200, gin.H{
 		"server":  "Rabbit Server",
 		"version": "0.1a",
