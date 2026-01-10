5c25ce82abccfdadb28bb3453d1d7b86efc34dd8
insert <insert@insertapp.net>
2026-01-10 16:47:32 -0500

    fix: no username validation on /user

--- frontend/htmlrender.go
+++ frontend/htmlrender.go
@@ -1,6 +1,8 @@
 package frontend
 
 import (
+	"regexp"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +17,11 @@ type id struct {
 func GenUser(c *gin.Context) {
 	var user id
 	c.BindUri(&user)
+	match, _ := regexp.MatchString(`^[a-zA-Z]+$`, user.Id)
+	if !match {
+		c.AbortWithStatusJSON(406, gin.H{"error": "Bad Username"})
+		return
+	}
 	c.HTML(200, "user.tmpl", gin.H{
 		"user": user.Id,
 	})
